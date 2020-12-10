b350c17cbab001eb9f40835f0cbd9fc17cccbee4
Umer Aziz <62507205+Umer-Aziz@users.noreply.github.com>
2020-12-10 05:51:42 +0500

    Update Weather_App.py

--- Weather_App.py
+++ Weather_App.py
@@ -1,10 +1,8 @@
 #importing request module to work with API.
-#it is use to send all kinds of HTTP requests. 
 import requests
 
-#Defining a Function.
 def GetWeather():
-    #getting Input city name from User.
+   
     CityName=input("Enter the City Name: ")
     
     #Create Acount On  https://home.openweathermap.org/users/sign_in and Enter your API KEY here.
@@ -20,12 +18,12 @@ def GetWeather():
     City_Temprature=Weatherdata["main"]["temp"]
     City_humidity=Weatherdata["main"]["humidity"]
 
-    #Printing the weather Detail.
+    
     
     print(f"\nMain Weather Condition =>> {MainWeather_Data}")
     #getting City Temp in Kelvin.
     print(f"The Temprature in Kelvin ==> {City_Temprature}")   
     print(f"The Humidity in air is ==> {City_humidity}")
     print(f"Description =>> {Description_data}")
-# Calling the Function.
+
 GetWeather()
